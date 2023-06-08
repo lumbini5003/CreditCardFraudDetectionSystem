11f780d6259215d7e7aabc4bc4b61917e93bd88b
lumbini5003 <lumbini.techmyster@gmail.com>
2023-06-08 12:46:19 +0530

    Second checkin

--- src/full_code.py
+++ src/full_code.py
@@ -1,3 +1,11 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing
 
+MAX_ROWs = 10000
+
+#Load Train data
+train_identity_df = pd.read_csv("/data/raw/train_identity.csv", nrows = MAX_ROWs)
+train_transaction_df = pd.read_csv("/data/raw/train_transaction.csv", nrows = MAX_ROWs)
+
+#Check data
+train_identity_df.head()
